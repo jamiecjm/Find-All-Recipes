a17e5b0426f80891ff0a35400a045004a43ca574
jiemeechong <jamiecjm@gmail.com>
2018-05-22 13:54:41 +1000

    Change fog provider

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,7 @@
 if Rails.env.production?
   require 'carrierwave/orm/activerecord'
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider => 'AWS',
       :aws_access_key_id => ENV['AWS_KEY'],
